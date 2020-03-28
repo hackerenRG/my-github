5d186c03b2c1d310e4c5cbfda8c5a6073ca433ff
renlanhui <renlanhui@ruijie.com.cn>
2020-03-28 14:43:11 +0800

    Add test.c

---
+++ test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: renlanhui
+	> Mail: renlanhui@ruijie.com.cn 
+	> Created Time: 六  3/28 14:42:09 2020
+ ************************************************************************/
+
+#include<stdio.h>
+
+int test_main(int argc, char *argv)
+{
+	printf("this is a main of unit-test");
+}
